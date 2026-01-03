db9b12d358b455a7e6c8b51c857d263c6654d6eb
warclib <36675461+warclib@users.noreply.github.com>
2026-01-02 22:30:21 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,20 @@
-const CACHE_NAME = 'otimac-v2'; // Cambiamos a v2 para forzar la actualización
+const CACHE_NAME = 'otimac-v3'; // Subimos a v3 para que el cambio sea inmediato
 const ASSETS = [
   './',
   './chofer.html',
   './cliente.html',
   './manifest-chofer.json',
-  './manifest-cliente.json', // <--- Faltaba este archivo en la lista
-  './logo.png'
+  './manifest-cliente.json',
+  './logo.png',    // Imagen del Chofer
+  './cliente.png'  // Imagen del Cliente (Agréga esta línea)
 ];
 
-// Instalación del Service Worker
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Estrategia de respuesta (Offline)
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((res) => res || fetch(e.request))
